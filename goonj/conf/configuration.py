acb02900b574cf46f1efcc9f4fade90fbb1b8dbf
keshrisohit <sumitk002@gmail.com>
2018-04-03 04:43:30 +0530

    Fixed exception handling

--- goonj/conf/configuration.py
+++ goonj/conf/configuration.py
@@ -91,28 +91,9 @@ class Configuration(object):
             with open(file_path, 'r') as stream:
                 loaded_config = yaml.load(stream)
                 self.set_config(**loaded_config)
-        except FileNotFoundError as e:
+        except Exception as e:
             traceback.print_exc()
             logger.error(
                 "Unable to load config file: {0} with exception {1}".format(
                     str(file_path), e))
-            self.load_with_defaults()
-            self.dump_to_file(file_path)
-
-    def dump_to_file(self, file_path):
-        logger = logging.getLogger(self.__class__.__name__)
-        conf_dict = {
-            "app_id": self.app_id,
-            "import_paths": self.import_paths,
-            "db_settings": self.db_setting.get_settings(),
-        }
-
-        try:
-            config_file = open(file_path, "w")
-            yaml.safe_dump(conf_dict, config_file, default_flow_style=False)
-            config_file.close()
-        except Exception as e:
-            traceback.print_exc()
-            logger.error(
-                "Unable to dump config file: {0} with exception {1}".format(
-                    str(file_path), e.message))
+            raise e
